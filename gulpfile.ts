841dce165f910d9cafba15aae9c32494f4b2075b
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-09-09 23:09:08 +0900

    Updated webserver and watching files

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,21 +1,32 @@
 import del from "del";
-import { series, src, dest, parallel } from "gulp";
+import { series, src, dest, parallel, watch } from "gulp";
 import pug from "gulp-pug";
+// @ts-ignore
+import ws from "gulp-webserver";
 
 const routes = {
   pug: {
     src: "src/*.pug",
     dest: "dist",
+    watch: "src/**/*.pug",
   },
 };
 
 const clean = async () => await del(["dist"]);
 
+const prepare = series(clean);
+
 const pugTask = () =>
   src(routes.pug.src).pipe(pug()).pipe(dest(routes.pug.dest));
 
-const prepare = series(clean);
-
 const assets = parallel(pugTask);
 
-export const dev = series(prepare, assets);
+const webserver = () => src("dist").pipe(ws({ livereload: true, open: true }));
+
+const watchTask = () => {
+  watch([routes.pug.watch], pugTask);
+};
+
+const postDev = parallel(webserver, watchTask);
+
+export const dev = series(prepare, assets, postDev);
