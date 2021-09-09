466234cbe5ffa11f5bc45250b85a6dac19214845
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-09-09 19:26:22 +0900

    Updated Pug Task

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,1 +1,21 @@
-import gulp from "gulp";
+import del from "del";
+import { series, src, dest, parallel } from "gulp";
+import pug from "gulp-pug";
+
+const routes = {
+  pug: {
+    src: "src/*.pug",
+    dest: "dist",
+  },
+};
+
+const clean = async () => await del(["dist"]);
+
+const pugTask = () =>
+  src(routes.pug.src).pipe(pug()).pipe(dest(routes.pug.dest));
+
+const prepare = series(clean);
+
+const assets = parallel(pugTask);
+
+export const dev = series(prepare, assets);
